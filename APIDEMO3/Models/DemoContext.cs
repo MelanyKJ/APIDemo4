c95411128b8d2bb5691cd97084f44524b1374a80
Diego Riveros <diego.riveros@tecsup.edu.pe>
2023-06-19 20:24:23 -0500

    Agregar archivos de proyecto.

---
+++ APIDEMO3/Models/DemoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIDEMO3.Models
+{
+    public class DemoContext : DbContext
+    {
+        public DemoContext(DbContextOptions<DemoContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+
+
+
+    }
+}
---
+++ APIDEMO3/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace APIDEMO3.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DNI { get; set; }
+    }
+}
